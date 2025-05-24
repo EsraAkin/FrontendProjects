27398619c2e1b1e43ba65c0a3719a0a0f12aab5e
Esra Akineden <e.akineden@gmail.com>
2025-05-25 00:34:40 +0200

    search axios und unsplash baslangic

---
+++ imagesSearch/src/SearchHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function SearchHeader() {
+  return (
+    <div className='searchDiv'>
+        <form>
+            <label>Ne arıyorsun?</label>
+            <input type='text'></input>
+        </form>
+    </div>
+  )
+}
